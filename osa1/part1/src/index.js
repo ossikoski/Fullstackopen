ecbafefcc1df9e7ec6e6868f9e263e892f32239c
ossikoski <ossioskari@gmail.com>
2020-04-03 17:21:18 +0300

    osa 1c

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
@@ -12,7 +15,7 @@ const Hello = (props) => {
 const App = () => {
   const nimi = 'Pekka'
   const ika = 10
-  console.log('Hello from komponentti')
+
   return (
     <>
       <h1>Greetings</h1>
