2be97b90312c9a1e2101b10e0ddaa477e1cdfbb2
ossikoski <ossioskari@gmail.com>
2020-04-05 19:22:42 +0300

    1.8 unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -17,11 +17,7 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   let all = good + neutral + bad
   let average = 0
   let positive = 0
@@ -29,15 +25,8 @@ const App = () => {
     average = (good - bad) / all
     positive = good / all * 100
   }
-    
-  return (
-    
+  return(
     <div>
-      <Header headerText='give feedback' />
-      <Button onClick={() => setGood(good + 1)} text='good'/>
-      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
-      <Button onClick={() => setBad(bad + 1)} text='bad'/>
-      <Header headerText='statistics' />
       {'good ' + good}
       <br></br>
       {'neutral ' + neutral}
@@ -49,6 +38,26 @@ const App = () => {
       {'average ' + average}
       <br></br>
       {'positive ' + positive + ' %'}
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+    
+  return (
+    
+    <div>
+      <Header headerText='give feedback' />
+      <Button onClick={() => setGood(good + 1)} text='good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text='bad'/>
+      <Header headerText='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
