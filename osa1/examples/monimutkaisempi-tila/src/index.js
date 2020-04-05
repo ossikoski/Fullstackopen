262d01a00c75927694cbc4e82f66f24b05164c5e
ossikoski <ossioskari@gmail.com>
2020-04-05 16:57:09 +0300

    1d

---
+++ osa1/examples/monimutkaisempi-tila/src/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
