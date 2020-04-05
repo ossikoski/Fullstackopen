2a247d7fdc0f443ed1e262b022f6ef95645d24d9
ossikoski <ossioskari@gmail.com>
2020-04-05 17:09:45 +0300

    1d taulukon käsittelyä

--- osa1/examples/monimutkaisempi-tila/src/index.js
+++ osa1/examples/monimutkaisempi-tila/src/index.js
@@ -5,21 +5,16 @@ const App = (props) => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
+    setClicks({ ...clicks,left: clicks.left + 1})
+    setAll(allClicks.concat('L'))
   }
 
   const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
+    setClicks({...clicks, right: clicks.right + 1 })
+    setAll(allClicks.concat('R'))
   }
 
   return (
@@ -29,6 +24,7 @@ const App = (props) => {
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
         {clicks.right}
+        <p>{allClicks.join(' ')}</p>
       </div>
     </div>
   )
