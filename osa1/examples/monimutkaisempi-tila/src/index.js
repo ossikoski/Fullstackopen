3500268abaf03409d0f366dfd9ab87b7b76693bc
ossikoski <ossioskari@gmail.com>
2020-04-05 17:21:29 +0300

    1d Ehdollinen renderöinti

--- osa1/examples/monimutkaisempi-tila/src/index.js
+++ osa1/examples/monimutkaisempi-tila/src/index.js
@@ -1,6 +1,29 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+
 const App = (props) => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
@@ -21,10 +44,10 @@ const App = (props) => {
     <div>
       <div>
         {clicks.left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
+        <Button onClick={handleLeftClick} text = 'left' />
+        <Button onClick={handleRightClick} text = 'right' />
         {clicks.right}
-        <p>{allClicks.join(' ')}</p>
+        <History allClicks={allClicks} />
       </div>
     </div>
   )
