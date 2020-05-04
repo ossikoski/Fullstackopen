85ed2a558ce3bf117831beb0b81a8c6d1458be0b
ossikoski <ossioskari@gmail.com>
2020-05-04 19:31:36 +0300

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     const personsArray = persons.map(person => person.name)
@@ -21,11 +23,17 @@ const App = () => {
     
      
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -40,13 +48,20 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value = {newNumber}
+            onChange = {handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) =>
         <div key={person.name}>
-        {person.name}
+        {person.name} {person.number}
         </div> 
       )}
     </div>
