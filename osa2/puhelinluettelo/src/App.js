710dc0e3ca46449b0719c4c66dc54114d97fd3c0
ossikoski <ossioskari@gmail.com>
2020-06-14 13:33:36 +0300

    2.15: puhelinluettelo step7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -34,12 +34,15 @@ const App = () => {
     console.log(personsArray.includes(newName))
     if(personsArray.includes(newName))
       alert(`${newName} is already added to phonebook`)
-    else
-      setPersons(persons.concat(personObject))
-    
-     
-    setNewName('')
-    setNewNumber('')
+    else{
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
   }
 
   const handleNameChange = (event) => {
