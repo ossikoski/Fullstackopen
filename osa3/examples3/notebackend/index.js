6ba52d8c9ef0b900c9cbd93dcff59e2171729d23
ossikoski <ossioskari@gmail.com>
2020-09-14 16:50:34 +0300

    3a materiaalit

--- osa3/examples3/notebackend/index.js
+++ osa3/examples3/notebackend/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const app = express()   //luodaan muuttujaan app sijoitettava express-sovellusta vastaava olio
 
+app.use(express.json()) // Auttaa POST-pyynnön bodyssa olevan muistiinpanon JSON-muotoisten tietojen parseamisessa
+//  json-parserin toimintaperiaatteena on, että se ottaa pyynnön mukana olevan JSON-muotoisen datan, 
+// muuttaa sen Javascript-olioksi ja sijoittaa request-olion kenttään body ennen kuin routen käsittelijää kutsutaan.
 let notes = [
     {
       id: 1,
@@ -37,13 +40,66 @@ app.get('/', (req, res) => {                // ensimmäinen määrittelee tapaht
   // Palvelin vastaa HTTP-pyyntöön lähettämällä selaimelle vastaukseksi send:in parametrina olevan merkkijonon
   // Koska parametri on merkkijono, asettaa express vastauksessa content-type-headerin arvoksi text/html, statuskoodiksi tulee oletusarvoisesti 200.
   
-  app.get('/api/notes', (req, res) => {     // Routeista toinen määrittelee tapahtumankäsittelijän, joka hoitaa sovelluksen
-    res.json(notes)                         // polkuun /api/notes tulevia HTTP GET -pyyntöjä:
-  })
-  // lähettää HTTP-pyynnön vastaukseksi parametrina olevaa Javascript-olioa eli taulukkoa notes vastaavan JSON-muotoisen merkkijonon.
-  // Nyt stringify-muutos tapahtuu automaattisesti expressillä
 
+ // Routeista toinen määrittelee tapahtumankäsittelijän, joka hoitaa sovelluksen polkuun /api/notes tulevia HTTP GET -pyyntöjä:
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)    // lähettää HTTP-pyynnön vastaukseksi parametrina olevaa Javascript-olioa eli taulukkoa notes vastaavan JSON-muotoisen merkkijonon.
+  })                        // Nyt stringify-muutos tapahtuu automaattisesti expressillä
   
+
+  app.get('/api/notes/:id', (request, response) => {    // käsittelee kaikki HTTP GET -pyynnöt, jotka ovat muotoa /api/notes/JOTAIN (ei sisälly api/notes) 
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {                 
+        response.json(note)    
+      } 
+    else {
+    response.status(404).end()
+    // Koska vastaukseen ei nyt liity mitään dataa käytetään statuskoodin asettavan metodin status lisäksi metodia end 
+    // ilmoittamaan siitä, että pyyntöön tulee vastata ilman dataa.
+      }
+  })
+
+  // Poisto tapahtuu tekemällä HTTP DELETE -pyyntö resurssin urliin:
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+    // Jos poisto onnistuu, eli poistettava muistiinpano on olemassa, vastataan statuskoodilla 204 no content sillä mukaan ei lähetetä mitään dataa.
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+
+    //const note = request.body   // Tapahtumankäsittelijäfunktio pääsee dataan käsiksi olion request kentän body avulla.
+    //note.id = maxId + 1
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        // kentän oletusarvo on false (eli jos kenttä important puuttuu)
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
 
 const PORT = 3001   // sitoo muuttujaan app sijoitetun http-palvelimen kuuntelemaan porttiin 3001 tulevia HTTP-pyyntöjä:
 app.listen(PORT, () => {
