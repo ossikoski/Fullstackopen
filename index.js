fcfa962d00aa039117fafc2668b6bcdec90f9abc
ossikoski <ossioskari@gmail.com>
2020-09-14 19:34:07 +0300

    3.5: puhelinluettelon backend step 5 POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "id": 1,
@@ -48,11 +50,35 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
     console.log(id)
   
     response.status(204).end()
 })
+
+const generateId = () => {
+    return Math.floor(Math.random() * Math.floor(100000))
+  }
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log("body", body)
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'name or number missing' 
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
