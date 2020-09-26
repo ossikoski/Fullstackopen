df50b655421f8e96e3e4ba05beed63181c21ef44
ossikoski <ossioskari@gmail.com>
2020-09-26 10:46:44 +0300

    4.2 utils tehty, app ja controllers luotu mutta tyhjiä

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,9 +1,10 @@
 const http = require('http')
 const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -14,9 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-console.log(process.env.MONGODB_URI)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
@@ -39,7 +38,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
