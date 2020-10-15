21e5afb0cdca96aa2156ad3f7d32ad3594220faf
ossikoski <ossioskari@gmail.com>
2020-10-15 11:15:01 +0300

    4.18: blogilistan laajennus, step6 token-perusteinen kirjautuminen

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -16,8 +17,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
-
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
