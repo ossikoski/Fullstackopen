f921050d5da6694e38329fe11f8a4900b5c0f3ad
ossikoski <ossioskari@gmail.com>
2020-10-23 17:23:26 +0300

    5.14 ja 5.15 blogilistan testit, step 2 ja 3 view ja like

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,131 +3,70 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-
-//console.log("firstdiv", component.container)
-//console.log("infodiv", infoDiv.container)
-
-test('renders title and author but doesnt render url and likes', () => {
-    const user = {
-        username: 'hellas',
-        name: 'Arto Hellas',
-        id: '5f871654def46b1c5096592b'
-      }
-      
-      const blog = {
-        url: 'Testiblogin.url.com',
-        title: 'Testiblogin title',
-        author: 'Testiblogin author',
-        user: {
-          username: 'hellas',
-          name: 'Arto Hellas',
-          id: '5f871654def46b1c5096592b'
-        },
-        likes: 1
-      }
-      
-      const mockHandleLike = jest.fn()
-      
-      const component = render(
-        <Blog blog={blog} user={user} />
-      )
-      //console.log('component', component)
-      const firstDiv = component.container.querySelector('.noInfo')
-      const infoDiv = component.container.querySelector('.info')
-      
-  expect(component.container).toHaveTextContent(
-    'Testiblogin title'
-  )
-  expect(component.container).toHaveTextContent(
-    'Testiblogin author'
-  )
-  expect(firstDiv).not.toHaveTextContent(
-    'Testiblogin.url.com'
-  )
-  expect(firstDiv).not.toHaveTextContent(
-    'likes'
-  )
-  expect(firstDiv).not.toHaveStyle('display: none')
-})
-
-test('after clicking view, all info is rendered', () => {
-    const user = {
-        username: 'hellas',
-        name: 'Arto Hellas',
-        id: '5f871654def46b1c5096592b'
-      }
-      
-    const blog = {
+describe('Blog tests', () => {
+  const user = {
+    username: 'hellas',
+    name: 'Arto Hellas',
+    id: '5f871654def46b1c5096592b'
+  }
+    
+  const blog = {
     url: 'Testiblogin.url.com',
     title: 'Testiblogin title',
     author: 'Testiblogin author',
     user: {
-        username: 'hellas',
-        name: 'Arto Hellas',
-        id: '5f871654def46b1c5096592b'
+      username: 'hellas',
+      name: 'Arto Hellas',
+      id: '5f871654def46b1c5096592b'
     },
     likes: 1
-    }
+  }
     
-    const mockHandleLike = jest.fn()
-    
-    const component = render(
-      <Blog blog={blog} user={user} />
-    )
-    //console.log('component', component)
-    const firstDiv = component.container.querySelector('.noInfo')
-    const infoDiv = component.container.querySelector('.info')
-      
-  const button = component.getByText('view')
-  fireEvent.click(button)
-
-  expect(component.container).toHaveTextContent(
-    'Testiblogin title'
-  )
-  expect(component.container).toHaveTextContent(
-    'Testiblogin author'
-  )
-  expect(component.container).toHaveTextContent(
-    'Testiblogin.url.com'
-  )
-  expect(component.container).toHaveTextContent(
-    'likes'
-  )
-  expect(component.container).not.toHaveStyle('display: none')
-})
-
-test('after clicking like twice, like function is called twice', () => {
-    const user = {
-        username: 'hellas',
-        name: 'Arto Hellas',
-        id: '5f871654def46b1c5096592b'
-      }
-      
-    const blog = {
-    url: 'Testiblogin.url.com',
-    title: 'Testiblogin title',
-    author: 'Testiblogin author',
-    user: {
-        username: 'hellas',
-        name: 'Arto Hellas',
-        id: '5f871654def46b1c5096592b'
-    },
-    likes: 1
-    }
-    
+  test('renders title and author but doesnt render url and likes', () => {
     const mockHandleLike = jest.fn()
     
     const component = render(
       <Blog blog={blog} user={user} handleLike={mockHandleLike}/>
     )
-    //console.log('component', component)
     const firstDiv = component.container.querySelector('.noInfo')
-    const infoDiv = component.container.querySelector('.info')
-
-  const likeButton = component.getByText('like')
-  fireEvent.click(likeButton)
-  fireEvent.click(likeButton)
-
-  expect(mockHandleLike.mock.calls).toHaveLength(2)
+        
+    expect(component.container).toHaveTextContent('Testiblogin title')
+    expect(component.container).toHaveTextContent('Testiblogin author')
+    expect(firstDiv).not.toHaveTextContent('Testiblogin.url.com')
+    expect(firstDiv).not.toHaveTextContent('likes')
+    expect(firstDiv).not.toHaveStyle('display: none')
+  })
+    
+  test('after clicking view, all info is rendered', () => {    
+    const mockHandleLike = jest.fn()
+    
+    const component = render(
+      <Blog blog={blog} user={user} handleLike={mockHandleLike}/>
+    )
+        
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    
+    expect(component.container).toHaveTextContent('Testiblogin title')
+    expect(component.container).toHaveTextContent('Testiblogin author')
+    expect(component.container).toHaveTextContent('Testiblogin.url.com')
+    expect(component.container).toHaveTextContent('likes')
+    expect(component.container).not.toHaveStyle('display: none')
+  })
+    
+  test('after clicking like twice, like function is called twice', () => {
+    const mockHandleLike = jest.fn()
+    
+    const component = render(
+      <Blog blog={blog} user={user} handleLike={mockHandleLike}/>
+    )
+    
+    const likeButton = component.getByText('like')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+    
+    expect(mockHandleLike.mock.calls).toHaveLength(2)
+  })
 })
+
 
