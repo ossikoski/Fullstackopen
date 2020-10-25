f98540ea8add713a58209bd5e588efda28a5adbd
ossikoski <ossioskari@gmail.com>
2020-10-25 15:06:53 +0200

    5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,12 +1,35 @@
 describe('Blog app', function() {
-    beforeEach(function() {
-      cy.request('POST', 'http://localhost:3001/api/testing/reset')
-      cy.visit('http://localhost:3000')
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Arto Hellas',
+      username: 'hellas',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user) 
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function(){
+      cy.get('#usernameInput').type('hellas')
+      cy.get('#passwordInput').type('salainen')
+      cy.get('#loginButton').click()
+      cy.contains('logged in')
     })
-  
-    it('Login form is shown', function() {
-      cy.contains('username')
-      cy.contains('password')
-      cy.contains('login')
+
+    it('fails with wrong credentials', function(){
+      cy.get('#usernameInput').type('hellas')
+      cy.get('#passwordInput').type('wrong')
+      cy.get('#loginButton').click()
+      cy.get('.error').contains('wrong')
     })
   })
+  
+})
