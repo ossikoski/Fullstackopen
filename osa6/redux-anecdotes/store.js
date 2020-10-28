0e038d26925282f6cf750a6560b742caa73c7342
ossikoski <ossioskari@gmail.com>
2020-10-28 15:40:48 +0200

    6.9 anekdootit, step7

---
+++ osa6/redux-anecdotes/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+)
+
+export default store
