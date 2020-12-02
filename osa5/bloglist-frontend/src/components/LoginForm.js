cbe7012375eba025bea38c44401334a9df7c4128
ossikoski <ossioskari@gmail.com>
2020-12-02 16:36:24 +0200

    7.19-7.20 tyylit, LoginForm

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -1,29 +1,34 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
 
 const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
   return(
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          id='usernameInput'
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <br></br>
-        password
-        <input
-          id='passwordInput'
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button id="loginButton" type="submit">login</button>
-    </form>
+    <div>
+      <Form onSubmit={handleLogin}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <input
+            id='usernameInput'
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <br></br>
+          <Form.Label>password:</Form.Label>
+          <input
+            id='passwordInput'
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <br></br>
+          <Button variant="primary" id="loginButton" type="submit">login</Button>
+        </Form.Group>
+      </Form>
+    </div>
   )
 }
 
