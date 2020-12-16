266a73937f84f732613901139e20d3e3d66f3692
ossikoski <ossioskari@gmail.com>
2020-12-16 14:02:25 +0200

    8.5 genren kirjat

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks(author: String): [Book!]!
+      allBooks(author: String, genre: String): [Book!]!
       allAuthors: [Author!]!
   }
 `
@@ -110,10 +110,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if(!args.author){
+      if(!args.author && !args.genre){
         return books
       }
-      return books.filter(b => b.author === args.author)
+      if(!args.author && args.genre){
+        return books.filter(b => b.genres.includes(args.genre))
+      }
+      if(args.author && !args.genre){
+        return books.filter(b => b.author === args.author)
+      }
     },
     allAuthors: () => authors
   },
