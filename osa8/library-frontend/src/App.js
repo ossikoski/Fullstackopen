58a779b93fc9d8be0b781709bcd4fe33ead1ce51
ossikoski <ossioskari@gmail.com>
2020-12-29 22:51:37 +0200

    8.18 Kirjautuminen toteutettu

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -3,9 +3,36 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+
+  if(!token){
+    return (
+      <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('login')}>login</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <LoginForm
+        show={page === 'login'} setToken={setToken}
+      />
+
+      </div>
+    )
+  }
 
   return (
     <div>
