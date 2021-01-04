31139ce6f2a6cec73b6ca83fb03cb580de8a1d79
ossikoski <ossioskari@gmail.com>
2021-01-04 20:13:00 +0200

    8.19 genren kirjat, osa 1

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
-import { ALL_BOOKS } from '../queries'
+import { ALL_BOOKS, GENRE_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const [books, setBooks] = useState([])
+  const [wantedBooks, setWantedBooks] = useState([])
+  const [genre, setGenre] = useState('')
+
   const result = useQuery(ALL_BOOKS)
-  if (result.loading)  {
-    return <div>loading...</div>
+    
+  const genres = []
+  for(let book_index in books){
+    for(let genre_index in books[book_index].genres){
+      if(!genres.includes(books[book_index].genres[genre_index])){
+        genres.push(books[book_index].genres[genre_index])
+      }
+    }
   }
-  
-  const books = result.data.allBooks
+  console.log('genres', genres)
+
+  useEffect(() => {
+    console.log('Book useEffect', genre)
+    if(!result.loading){
+      setBooks(result.data.allBooks)
+      if(genre)
+        setWantedBooks(result.data.allBooks.filter(b => b.genres.includes(genre)))
+      else
+        setWantedBooks(result.data.allBooks)
+    }
+  }, [genre, result.data])
 
   if (!props.show) {
     return null
@@ -17,6 +37,15 @@ const Books = (props) => {
   return (
     <div>
       <h2>books</h2>
+
+      {
+        genre ? 
+          <div>
+            in genre {genre}
+          </div>
+        :
+          <div></div>
+      }
 
       <table>
         <tbody>
@@ -29,7 +58,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {wantedBooks.map(a =>
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -38,6 +67,12 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+
+      
+      {genres.map(g => 
+        <button key={g} onClick={() => setGenre(g)}>{g}</button>)
+      }
+      <button onClick={() => setGenre('')}>all genres</button>
     </div>
   )
 }
